565414c5870f376dd8da6ab45e2f1b7a5d9ccd3d
dogef <lovme.amir@gmail.com>
2021-06-02 01:02:28 +0400

    fixed unix bug

--- server.js
+++ server.js
@@ -34,15 +34,16 @@ app.get('/api', (req,res)=>{
 
 app.get("/api/:date",(req,res)=>{
   let dateString = req.params.date;
-  let passedInValue = new Date(dateString);
 
-  if (parseInt(dateString) > 1000){
+  if (parseInt(dateString) > 10000){
     let unixTime = new Date(parseInt(dateString));
     res.json({
       "unix":unixTime.getTime(),
       "utc": unixTime.toUTCString()
     })
   }
+
+  let passedInValue = new Date(dateString);
 
   if (passedInValue == "Invalid Date"){
     res.json({"error":"Invalid Date"});
